89296079890743336861371b408e9a3846319eee
Thomas Daubney <thomas.daubney@arm.com>
2024-08-16 09:01:17 +0100

    Generate plots
    Signed-off-by: Thomas Daubney <thomas.daubney@arm.com>

--- plot_data.py
+++ plot_data.py
@@ -3,9 +3,25 @@ import csv
 import sys
 import matplotlib.pyplot as plt
 
-OPENSSL_BASE_FILE_NAME = "./results/OpenSSL_v3-4-0dev_results.csv"
-OPENSSL_INTERLEAVING_FILE_NAME = "./results/OpenSSL_v3-4-0dev_interleaving_results.csv"
-AARCH64CRYPTOLIB_FILE_NAME = "./results/AArch64cryptolib_results.csv"
+OPENSSL_BASE_AES128_SHA1_FILE_NAME = "./results/OpenSSL_aes_cbc_128_sha1_hmac_encrypt.csv"
+OPENSSL_INTERLEAVING_AES128_SHA1_FILE_NAME = "./results/OpenSSL_interleaving_aes_cbc_128_sha1_hmac_encrypt.csv"
+AARCH64CRYPTOLIB_AES128_SHA1_FILE_NAME = "./results/AArch64cryptolib_aes_cbc_128_sha1_hmac_encrypt.csv"
+
+OPENSSL_BASE_AES128_SHA256_FILE_NAME = "./results/OpenSSL_aes_cbc_128_sha256_hmac_encrypt.csv"
+OPENSSL_INTERLEAVING_AES128_SHA256_FILE_NAME = "./results/OpenSSL_interleaving_aes_cbc_128_sha256_hmac_encrypt.csv"
+AARCH64CRYPTOLIB_AES128_SHA256_FILE_NAME = "./results/AArch64cryptolib_aes_cbc_128_sha256_hmac_encrypt.csv"
+
+OPENSSL_BASE_AES256_SHA1_FILE_NAME = "./results/OpenSSL_aes_cbc_256_sha1_hmac_encrypt.csv"
+OPENSSL_INTERLEAVING_AES256_SHA1_FILE_NAME = "./results/OpenSSL_interleaving_aes_cbc_256_sha1_hmac_encrypt.csv"
+
+OPENSSL_BASE_AES256_SHA256_FILE_NAME = "./results/OpenSSL_aes_cbc_256_sha256_hmac_encrypt.csv"
+OPENSSL_INTERLEAVING_AES256_SHA256_FILE_NAME = "./results/OpenSSL_interleaving_aes_cbc_256_sha256_hmac_encrypt.csv"
+
+OPENSSL_BASE_AES128_SHA512_FILE_NAME = "./results/OpenSSL_aes_cbc_128_sha512_hmac_encrypt.csv"
+OPENSSL_INTERLEAVING_AES128_SHA512_FILE_NAME = "./results/OpenSSL_interleaving_aes_cbc_128_sha512_hmac_encrypt.csv"
+
+OPENSSL_BASE_AES256_SHA512_FILE_NAME = "./results/OpenSSL_aes_cbc_256_sha512_hmac_encrypt.csv"
+OPENSSL_INTERLEAVING_AES256_SHA512_FILE_NAME = "./results/OpenSSL_interleaving_aes_cbc_256_sha512_hmac_encrypt.csv"
 
 OPENSSL_BASE_SERIES_NAME = "OpenSSL_base"
 OPENSSL_INTERLEAVING_SERIES_NAME = "OpenSSL_interleaving"
@@ -33,9 +49,9 @@ class DataSeries:
 def main():
     print("Plotting data...")
 
-    openssl_base_series = DataSeries(OPENSSL_BASE_FILE_NAME, OPENSSL_BASE_SERIES_NAME)
-    openssl_interleaving_series = DataSeries(OPENSSL_INTERLEAVING_FILE_NAME, OPENSSL_INTERLEAVING_SERIES_NAME)
-    aarch64cryptolib_series = DataSeries(AARCH64CRYPTOLIB_FILE_NAME, AARCH64CRYPTOLIB_SERIES_NAME)
+    openssl_base_series = DataSeries(OPENSSL_BASE_AES128_SHA1_FILE_NAME, OPENSSL_BASE_SERIES_NAME)
+    openssl_interleaving_series = DataSeries(OPENSSL_INTERLEAVING_AES128_SHA256_FILE_NAME, OPENSSL_INTERLEAVING_SERIES_NAME)
+    aarch64cryptolib_series = DataSeries(AARCH64CRYPTOLIB_AES128_SHA1_FILE_NAME, AARCH64CRYPTOLIB_SERIES_NAME)
 
 
     complete_data_series = [openssl_base_series, openssl_interleaving_series, aarch64cryptolib_series]
@@ -43,7 +59,7 @@ def main():
 
 
     for series, colour in zip(complete_data_series, colours):
-        plt.plot(series.x_data, series.y_data, label=series.series_name, marker='o', c=colour)
+        plt.plot(series.x_data, series.y_data, label=series.series_name, marker='.', markersize=3, c=colour)
     # plt.yticks = series.y_data
     plt.figure(1)
 
@@ -52,8 +68,255 @@ def main():
     plt.xlabel(X_DATA_LABEL)
     plt.ylabel(Y_DATA_LABEL)
     plt.legend()
-    plt.savefig("results.png", dpi=300)
-    plt.show()
+    plt.savefig("./plots/aes128_sha1.png", dpi=300)
+    plt.close()
+
+
+
+    openssl_base_series = DataSeries(OPENSSL_BASE_AES128_SHA256_FILE_NAME, OPENSSL_BASE_SERIES_NAME)
+    openssl_interleaving_series = DataSeries(OPENSSL_INTERLEAVING_AES128_SHA256_FILE_NAME, OPENSSL_INTERLEAVING_SERIES_NAME)
+    aarch64cryptolib_series = DataSeries(AARCH64CRYPTOLIB_AES128_SHA256_FILE_NAME, AARCH64CRYPTOLIB_SERIES_NAME)
+
+    complete_data_series = [openssl_base_series, openssl_interleaving_series, aarch64cryptolib_series]
+    colours = ['r', 'b', 'g']
+
+
+    for series, colour in zip(complete_data_series, colours):
+        plt.plot(series.x_data, series.y_data, label=series.series_name, marker='.', markersize=3, c=colour)
+    # plt.yticks = series.y_data
+    plt.figure(1)
+
+    plt.grid(True)
+    plt.title("AES_CBC_128_HMAC_SHA256 - Encrypt: Buffer size vs. Throughput")
+    plt.xlabel(X_DATA_LABEL)
+    plt.ylabel(Y_DATA_LABEL)
+    plt.legend()
+    plt.savefig("./plots/aes128_sha256.png", dpi=300)
+    plt.close()
+
+
+
+    openssl_base_series = DataSeries(OPENSSL_BASE_AES256_SHA1_FILE_NAME, OPENSSL_BASE_SERIES_NAME)
+    openssl_interleaving_series = DataSeries(OPENSSL_INTERLEAVING_AES256_SHA1_FILE_NAME, OPENSSL_INTERLEAVING_SERIES_NAME)
+
+    complete_data_series = [openssl_base_series, openssl_interleaving_series]
+    colours = ['r', 'b']
+
+
+    for series, colour in zip(complete_data_series, colours):
+        plt.plot(series.x_data, series.y_data, label=series.series_name, marker='.', markersize=3, c=colour)
+    # plt.yticks = series.y_data
+    plt.figure(1)
+
+    plt.grid(True)
+    plt.title("AES_CBC_256_HMAC_SHA1 - Encrypt: Buffer size vs. Throughput")
+    plt.xlabel(X_DATA_LABEL)
+    plt.ylabel(Y_DATA_LABEL)
+    plt.legend()
+    plt.savefig("./plots/aes256_sha1.png", dpi=300)
+    plt.close()
+
+
+
+    openssl_base_series = DataSeries(OPENSSL_BASE_AES256_SHA256_FILE_NAME, OPENSSL_BASE_SERIES_NAME)
+    openssl_interleaving_series = DataSeries(OPENSSL_INTERLEAVING_AES256_SHA256_FILE_NAME, OPENSSL_INTERLEAVING_SERIES_NAME)
+
+    complete_data_series = [openssl_base_series, openssl_interleaving_series]
+    colours = ['r', 'b']
+
+
+    for series, colour in zip(complete_data_series, colours):
+        plt.plot(series.x_data, series.y_data, label=series.series_name, marker='.', markersize=3, c=colour)
+    # plt.yticks = series.y_data
+    plt.figure(1)
+
+    plt.grid(True)
+    plt.title("AES_CBC_256_HMAC_SHA256 - Encrypt: Buffer size vs. Throughput")
+    plt.xlabel(X_DATA_LABEL)
+    plt.ylabel(Y_DATA_LABEL)
+    plt.legend()
+    plt.savefig("./plots/aes256_sha256.png", dpi=300)
+    plt.close()
+
+
+
+    openssl_base_series = DataSeries(OPENSSL_BASE_AES128_SHA512_FILE_NAME, OPENSSL_BASE_SERIES_NAME)
+    openssl_interleaving_series = DataSeries(OPENSSL_INTERLEAVING_AES128_SHA512_FILE_NAME, OPENSSL_INTERLEAVING_SERIES_NAME)
+
+    complete_data_series = [openssl_base_series, openssl_interleaving_series]
+    colours = ['r', 'b']
+
+
+    for series, colour in zip(complete_data_series, colours):
+        plt.plot(series.x_data, series.y_data, label=series.series_name, marker='.', markersize=3, c=colour)
+    # plt.yticks = series.y_data
+    plt.figure(1)
+
+    plt.grid(True)
+    plt.title("AES_CBC_128_HMAC_SHA512 - Encrypt: Buffer size vs. Throughput")
+    plt.xlabel(X_DATA_LABEL)
+    plt.ylabel(Y_DATA_LABEL)
+    plt.legend()
+    plt.savefig("./plots/aes128_sha512.png", dpi=300)
+    plt.close()
+
+
+
+    openssl_base_series = DataSeries(OPENSSL_BASE_AES256_SHA512_FILE_NAME, OPENSSL_BASE_SERIES_NAME)
+    openssl_interleaving_series = DataSeries(OPENSSL_INTERLEAVING_AES256_SHA512_FILE_NAME, OPENSSL_INTERLEAVING_SERIES_NAME)
+
+    complete_data_series = [openssl_base_series, openssl_interleaving_series]
+    colours = ['r', 'b']
+
+
+    for series, colour in zip(complete_data_series, colours):
+        plt.plot(series.x_data, series.y_data, label=series.series_name, marker='.', markersize=3, c=colour)
+    # plt.yticks = series.y_data
+    plt.figure(1)
+
+    plt.grid(True)
+    plt.title("AES_CBC_256_HMAC_SHA512 - Encrypt: Buffer size vs. Throughput")
+    plt.xlabel(X_DATA_LABEL)
+    plt.ylabel(Y_DATA_LABEL)
+    plt.legend()
+    plt.savefig("./plots/aes256_sha512.png", dpi=300)
+    plt.close()
+
+
+########################################## Zoomed plots ##########################################
+
+    openssl_base_series = DataSeries(OPENSSL_BASE_AES128_SHA1_FILE_NAME, OPENSSL_BASE_SERIES_NAME)
+    openssl_interleaving_series = DataSeries(OPENSSL_INTERLEAVING_AES128_SHA256_FILE_NAME, OPENSSL_INTERLEAVING_SERIES_NAME)
+    aarch64cryptolib_series = DataSeries(AARCH64CRYPTOLIB_AES128_SHA1_FILE_NAME, AARCH64CRYPTOLIB_SERIES_NAME)
+
+
+    complete_data_series = [openssl_base_series, openssl_interleaving_series, aarch64cryptolib_series]
+    colours = ['r', 'b', 'g']
+
+
+    for series, colour in zip(complete_data_series, colours):
+        plt.plot(series.x_data[0:6], series.y_data[0:6], label=series.series_name, marker='.', markersize=3, c=colour)
+    # plt.yticks = series.y_data
+    plt.figure(1)
+
+    plt.grid(True)
+    plt.title("AES_CBC_128_HMAC_SHA1 - Encrypt: Buffer size vs. Throughput")
+    plt.xlabel(X_DATA_LABEL)
+    plt.ylabel(Y_DATA_LABEL)
+    plt.legend()
+    plt.savefig("./plots/aes128_sha1_zoomed.png", dpi=300)
+    plt.close()
+
+
+
+    openssl_base_series = DataSeries(OPENSSL_BASE_AES128_SHA256_FILE_NAME, OPENSSL_BASE_SERIES_NAME)
+    openssl_interleaving_series = DataSeries(OPENSSL_INTERLEAVING_AES128_SHA256_FILE_NAME, OPENSSL_INTERLEAVING_SERIES_NAME)
+    aarch64cryptolib_series = DataSeries(AARCH64CRYPTOLIB_AES128_SHA256_FILE_NAME, AARCH64CRYPTOLIB_SERIES_NAME)
+
+    complete_data_series = [openssl_base_series, openssl_interleaving_series, aarch64cryptolib_series]
+    colours = ['r', 'b', 'g']
+
+
+    for series, colour in zip(complete_data_series, colours):
+        plt.plot(series.x_data[0:6], series.y_data[0:6], label=series.series_name, marker='.', markersize=3, c=colour)
+    # plt.yticks = series.y_data
+    plt.figure(1)
+
+    plt.grid(True)
+    plt.title("AES_CBC_128_HMAC_SHA256 - Encrypt: Buffer size vs. Throughput")
+    plt.xlabel(X_DATA_LABEL)
+    plt.ylabel(Y_DATA_LABEL)
+    plt.legend()
+    plt.savefig("./plots/aes128_sha256_zoomed.png", dpi=300)
+    plt.close()
+
+
+
+    openssl_base_series = DataSeries(OPENSSL_BASE_AES256_SHA1_FILE_NAME, OPENSSL_BASE_SERIES_NAME)
+    openssl_interleaving_series = DataSeries(OPENSSL_INTERLEAVING_AES256_SHA1_FILE_NAME, OPENSSL_INTERLEAVING_SERIES_NAME)
+
+    complete_data_series = [openssl_base_series, openssl_interleaving_series]
+    colours = ['r', 'b']
+
+
+    for series, colour in zip(complete_data_series, colours):
+        plt.plot(series.x_data[0:6], series.y_data[0:6], label=series.series_name, marker='.', markersize=3, c=colour)
+    # plt.yticks = series.y_data
+    plt.figure(1)
+
+    plt.grid(True)
+    plt.title("AES_CBC_256_HMAC_SHA1 - Encrypt: Buffer size vs. Throughput")
+    plt.xlabel(X_DATA_LABEL)
+    plt.ylabel(Y_DATA_LABEL)
+    plt.legend()
+    plt.savefig("./plots/aes256_sha1_zoomed.png", dpi=300)
+    plt.close()
+
+
+
+    openssl_base_series = DataSeries(OPENSSL_BASE_AES256_SHA256_FILE_NAME, OPENSSL_BASE_SERIES_NAME)
+    openssl_interleaving_series = DataSeries(OPENSSL_INTERLEAVING_AES256_SHA256_FILE_NAME, OPENSSL_INTERLEAVING_SERIES_NAME)
+
+    complete_data_series = [openssl_base_series, openssl_interleaving_series]
+    colours = ['r', 'b']
+
+
+    for series, colour in zip(complete_data_series, colours):
+        plt.plot(series.x_data[0:6], series.y_data[0:6], label=series.series_name, marker='.', markersize=3, c=colour)
+    # plt.yticks = series.y_data
+    plt.figure(1)
+
+    plt.grid(True)
+    plt.title("AES_CBC_256_HMAC_SHA256 - Encrypt: Buffer size vs. Throughput")
+    plt.xlabel(X_DATA_LABEL)
+    plt.ylabel(Y_DATA_LABEL)
+    plt.legend()
+    plt.savefig("./plots/aes256_sha256_zoomed.png", dpi=300)
+    plt.close()
+
+
+
+    openssl_base_series = DataSeries(OPENSSL_BASE_AES128_SHA512_FILE_NAME, OPENSSL_BASE_SERIES_NAME)
+    openssl_interleaving_series = DataSeries(OPENSSL_INTERLEAVING_AES128_SHA512_FILE_NAME, OPENSSL_INTERLEAVING_SERIES_NAME)
+
+    complete_data_series = [openssl_base_series, openssl_interleaving_series]
+    colours = ['r', 'b']
+
+
+    for series, colour in zip(complete_data_series, colours):
+        plt.plot(series.x_data[0:6], series.y_data[0:6], label=series.series_name, marker='.', markersize=3, c=colour)
+    # plt.yticks = series.y_data
+    plt.figure(1)
+
+    plt.grid(True)
+    plt.title("AES_CBC_128_HMAC_SHA512 - Encrypt: Buffer size vs. Throughput")
+    plt.xlabel(X_DATA_LABEL)
+    plt.ylabel(Y_DATA_LABEL)
+    plt.legend()
+    plt.savefig("./plots/aes128_sha512_zoomed.png", dpi=300)
+    plt.close()
+
+
+
+    openssl_base_series = DataSeries(OPENSSL_BASE_AES256_SHA512_FILE_NAME, OPENSSL_BASE_SERIES_NAME)
+    openssl_interleaving_series = DataSeries(OPENSSL_INTERLEAVING_AES256_SHA512_FILE_NAME, OPENSSL_INTERLEAVING_SERIES_NAME)
+
+    complete_data_series = [openssl_base_series, openssl_interleaving_series]
+    colours = ['r', 'b']
+
+
+    for series, colour in zip(complete_data_series, colours):
+        plt.plot(series.x_data[0:6], series.y_data[0:6], label=series.series_name, marker='.', markersize=3, c=colour)
+    # plt.yticks = series.y_data
+    plt.figure(1)
+
+    plt.grid(True)
+    plt.title("AES_CBC_256_HMAC_SHA512 - Encrypt: Buffer size vs. Throughput")
+    plt.xlabel(X_DATA_LABEL)
+    plt.ylabel(Y_DATA_LABEL)
+    plt.legend()
+    plt.savefig("./plots/aes256_sha512_zoomed.png", dpi=300)
+    plt.close()
 
 if __name__ == "__main__":
     sys.exit(main())
